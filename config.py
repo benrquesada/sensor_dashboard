de461be43a73622b0c2c16686ba1847a39cffcc4
guillermo <guillermoramos330179@gmail.com>
2016-10-31 19:50:56 +0000

    add email config

---
+++ config.py
@@ -0,0 +1,10 @@
+# email server
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'notificationspmssthermostat'
+MAIL_PASSWORD = 'pmss2000'
+
+# administrator list
+ADMINS = ['hunterr@berea.edu']
