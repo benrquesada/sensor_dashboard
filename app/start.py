a7c49f118a3b0c31a08594d14d3678a17fdb3ac3
Rebeccah Hunter <rebeccah.hunter@gmail.com>
2016-10-31 19:40:15 +0000

    "fixed models"

--- app/start.py
+++
@@ -1,6 +0,0 @@
-# this renders the home page which is start.html
-from allImports import *
-@app.route("/<key>/<id>/<tag>/<value>", methods = ["GET"])
-def start(key, id, tag, value):
-  print ("Key: {0} ID: {1} Tag: {2} Value: {3}".format(key, id, tag, value))
-  return "Sucess!!!"                       
